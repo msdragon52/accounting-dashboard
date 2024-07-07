558506f1765d537f9b07573fe23350bce9371e94
Michael Bellias <michaelbellias@mac-mini.mynetworksettings.com>
2024-07-07 15:28:50 -0400

    refactored navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,18 +2,41 @@ import Link from 'next/link';
 import { Separator } from './ui/separator';
 
 export default function Navbar() {
+  const navLinks = [
+    {
+      label: 'Home',
+      ref: '',
+    },
+    {
+      label: 'About',
+      ref: '#',
+    },
+    {
+      label: 'Upgrade',
+      ref: '#',
+    },
+    {
+      label: 'FAQs',
+      ref: '#',
+    },
+    {
+      label: 'Support',
+      ref: '#',
+    },
+  ];
+
   return (
     <nav className='p-8 w-[15rem] text-white bg-blue-500 border-r border-white'>
       <ul className='flex flex-col items-center justify-center'>
         <Link
           href='/login'
-          className='m-4'
+          className='m-4 hover:underline'
         >
           Login
         </Link>
         <Link
           href='/sign-up'
-          className='m-4'
+          className='m-4 hover:underline'
         >
           Sign Up
         </Link>
@@ -21,36 +44,15 @@ export default function Navbar() {
           orientation='horizontal'
           className='w-6 m-8'
         />
-        <Link
-          href='/'
-          className='m-4'
-        >
-          Home
-        </Link>
-        <Link
-          href='/sign-up'
-          className='m-4'
-        >
-          About
-        </Link>
-        <Link
-          href='/sign-up'
-          className='m-4'
-        >
-          Upgrades
-        </Link>
-        <Link
-          href='/sign-up'
-          className='m-4'
-        >
-          FAQs
-        </Link>
-        <Link
-          href='/sign-up'
-          className='m-4'
-        >
-          Support
-        </Link>
+        {navLinks.map(({ label, ref }, index) => (
+          <Link
+            key={index}
+            className='m-4 hover:underline'
+            href={`/${ref}`}
+          >
+            {label}
+          </Link>
+        ))}
       </ul>
     </nav>
   );
