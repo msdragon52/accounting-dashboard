5f161747198c2ad0f957308ed93f3a4ea73caf7a
Michael Bellias <michaelbellias@mac-mini.mynetworksettings.com>
2024-07-07 15:32:29 -0400

    refactored navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,14 @@ import { Separator } from './ui/separator';
 export default function Navbar() {
   const navLinks = [
     {
+      label: 'Login',
+      ref: 'login',
+    },
+    {
+      label: 'Sign Up',
+      ref: 'sign-up',
+    },
+    {
       label: 'Home',
       ref: '',
     },
@@ -28,30 +36,22 @@ export default function Navbar() {
   return (
     <nav className='p-8 w-[15rem] text-white bg-blue-500 border-r border-white'>
       <ul className='flex flex-col items-center justify-center'>
-        <Link
-          href='/login'
-          className='m-4 hover:underline'
-        >
-          Login
-        </Link>
-        <Link
-          href='/sign-up'
-          className='m-4 hover:underline'
-        >
-          Sign Up
-        </Link>
-        <Separator
-          orientation='horizontal'
-          className='w-6 m-8'
-        />
         {navLinks.map(({ label, ref }, index) => (
-          <Link
-            key={index}
-            className='m-4 hover:underline'
-            href={`/${ref}`}
-          >
-            {label}
-          </Link>
+          <>
+            <Link
+              key={index}
+              className='m-4 hover:underline'
+              href={`/${ref}`}
+            >
+              {label}
+            </Link>
+            {label === 'Sign Up' && (
+              <Separator
+                orientation='horizontal'
+                className='w-6 m-8'
+              />
+            )}
+          </>
         ))}
       </ul>
     </nav>
